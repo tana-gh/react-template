1642f8a5ef41d25cc189879e0527fda2df93276c
tana-gh <yozoranotakaku@gmail.com>
2020-03-22 15:51:16 +0900

    Change sass to scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import ReactDOM   from 'react-dom'
 import App        from './components/App'
-import '../assets/sass/style.sass'
+import '../assets/scss/style.scss'
 
 ReactDOM.render(<App/>, document.getElementById('app'))
